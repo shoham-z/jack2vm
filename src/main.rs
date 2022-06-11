57c64dae339edb74bdae6f838bf3202718264e7e
Shoham <shozeh123@gmail.com>
2022-06-12 00:59:48 +0300

    modified for easier testing

--- src/main.rs
+++ src/main.rs
@@ -21,52 +21,37 @@ fn main() {
     //let args: Vec<String> = env::args().collect();
 
 
-    let mut file_path;
+    let mut file_path:Vec<String> = Vec::new();
     //let temp = args[1].to_string();
+    let compile_all = true;
 
-    //let temp ="/home/shoham/nand2tetris/projects/11/Seven/".to_string();
-    //let temp ="/home/shoham/nand2tetris/projects/11/Average/".to_string();
-    //let temp ="/home/shoham/nand2tetris/projects/11/ConvertToBin/".to_string();
-    //let temp ="/home/shoham/nand2tetris/projects/11/ComplexArrays/".to_string();
-    //let temp ="/home/shoham/nand2tetris/projects/11/Square/".to_string();
-    //let temp ="/home/shoham/nand2tetris/projects/11/Pong/".to_string();
+    if compile_all {
+        file_path.append(&mut search_jack_files("/home/shoham/nand2tetris/projects/11/Seven/"));
+        file_path.append(&mut search_jack_files("/home/shoham/nand2tetris/projects/11/Average/"));
+        file_path.append(&mut search_jack_files("/home/shoham/nand2tetris/projects/11/ConvertToBin/"));
+        file_path.append(&mut search_jack_files("/home/shoham/nand2tetris/projects/11/ComplexArrays/"));
+        file_path.append(&mut search_jack_files("/home/shoham/nand2tetris/projects/11/Square/"));
+        file_path.append(&mut search_jack_files("/home/shoham/nand2tetris/projects/11/Pong/"));
 
-    let temp = "/home/shoham/nand2tetris/projects/09/game/".to_string();
+        for file in file_path {
+            println!("file : {}", file);
+            tokenizer(file.to_string());
+            let mut compilation_engine: CompilationEngine = CompilationEngine::new(&file);
+            compilation_engine.compile();
+        }
+    } else {
+        let temp = "/home/shoham/nand2tetris/projects/11/Pong/Ball.jack".to_string();
 
-    if !temp.contains(".jack") { file_path = search_jack_files(temp.as_str()); } else {
-        file_path = Vec::new();
-        file_path.push(temp.to_string())
-    }
-    for file in file_path {
-        println!("file : {}", file);
-        tokenizer(file.to_string());
-        let mut compilation_engine: CompilationEngine = CompilationEngine::new(&file);
-        compilation_engine.compile();
-        //compilation_engine.class_name = "Main".to_string();
-        /*compilation_engine.compile_subroutine_dec("function void convert(int value) {
-    	var int mask, position;
-    	var boolean loop;
-
-    	let loop = true;
-    	while (loop) {
-    	    let position = position + 1;
-    	    let mask = Main.nextMask(mask);
-
-    	    if (~(position > 16)) {
-
-    	        if (~((value & mask) = 0)) {
-    	            do Memory.poke(8000 + position, 1);
-       	        }
-    	        else {
-    	            do Memory.poke(8000 + position, 0);
-      	        }
-    	    }
-    	    else {
-    	        let loop = false;
-    	    }
-    	}
-    	return;
-    }".to_string());*/
+        if !temp.contains(".jack") { file_path = search_jack_files(temp.as_str()); } else {
+            file_path = Vec::new();
+            file_path.push(temp.to_string())
+        }
+        for file in file_path {
+            println!("file : {}", file);
+            tokenizer(file.to_string());
+            let mut compilation_engine: CompilationEngine = CompilationEngine::new(&file);
+            compilation_engine.compile();
+        }
     }
 }
 
